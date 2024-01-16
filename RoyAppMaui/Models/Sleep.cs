2258ecaedb58d69fcab13bd38547f6ca46e62b9b
jdubar <55170139+jdubar@users.noreply.github.com>
2024-01-16 18:26:30 -0500

    Add project files.

---
+++ RoyAppMaui/Models/Sleep.cs
@@ -0,0 +1,10 @@
+﻿namespace RoyAppMaui.Models;
+public class Sleep
+{
+    public int Id { get; set; }
+    public DateTime Bedtime { get; set; }
+    public decimal BedtimeRec { get; set; }
+    public DateTime Waketime { get; set; }
+    public decimal WaketimeRec { get; set; }
+    public decimal Duration { get; set; }
+}
