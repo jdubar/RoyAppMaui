1c2b3bb84b944d605d67f6f0fd93767d0aa07aec
jdubar <55170139+jdubar@users.noreply.github.com>
2024-01-17 09:59:55 -0500

    Updated main layout

---
+++ RoyAppMaui/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,29 @@
+﻿using MudBlazor;
+
+namespace RoyAppMaui.Components.Layout;
+public partial class MainLayout
+{
+    private bool _drawerOpen = false;
+    private bool _isDarkMode;
+    private MudThemeProvider _mudThemeProvider;
+
+    private void DrawerToggle() =>
+        _drawerOpen = !_drawerOpen;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _isDarkMode = await _mudThemeProvider.GetSystemPreference();
+            await _mudThemeProvider.WatchSystemPreference(OnSystemPreferenceChanged);
+            StateHasChanged();
+        }
+    }
+
+    private Task OnSystemPreferenceChanged(bool newValue)
+    {
+        _isDarkMode = newValue;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+}
