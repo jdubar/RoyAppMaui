feb1aef18ebfb3e54e6ebd40cb62880707aedbf6
jdubar <55170139+jdubar@users.noreply.github.com>
2024-01-16 22:27:29 -0500

    Updated list view

--- RoyAppMaui/Models/Sleep.cs
+++ RoyAppMaui/Models/Sleep.cs
@@ -2,9 +2,9 @@
 public class Sleep
 {
     public int Id { get; set; }
-    public DateTime Bedtime { get; set; }
+    public TimeSpan? Bedtime { get; set; } = new TimeSpan(0, 0, 0);
     public decimal BedtimeRec { get; set; }
-    public DateTime Waketime { get; set; }
+    public TimeSpan? Waketime { get; set; } = new TimeSpan(0, 0, 0);
     public decimal WaketimeRec { get; set; }
     public decimal Duration { get; set; }
 }
