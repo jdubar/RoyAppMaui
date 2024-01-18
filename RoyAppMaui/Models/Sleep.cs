d8162f1f4344b2bfd5444f5fad4898cc0d811d6d
jdubar <55170139+jdubar@users.noreply.github.com>
2024-01-18 13:35:05 -0500

    Got edit functional now for tests

--- RoyAppMaui/Models/Sleep.cs
+++ RoyAppMaui/Models/Sleep.cs
@@ -4,9 +4,9 @@ public record Sleep
     public int Id { get; set; }
     public TimeSpan? Bedtime { get; set; } = new TimeSpan(0, 0, 0);
     public decimal BedtimeRec { get; set; }
-    public string BedtimeDisplay { get; set; } = string.Empty;
+    public string BedtimeDisplay { get; set; } = "12:00 AM";
     public TimeSpan? Waketime { get; set; } = new TimeSpan(0, 0, 0);
     public decimal WaketimeRec { get; set; }
-    public string WaketimeDisplay { get; set; } = string.Empty;
+    public string WaketimeDisplay { get; set; } = "12:00 AM";
     public decimal Duration { get; set; }
 }
