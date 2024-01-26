5b1aa8fbd1715496a4df337368aba9468337561d
jdubar <55170139+jdubar@users.noreply.github.com>
2024-01-25 20:01:01 -0500

    Added ParseImportFileData to file service

--- RoyAppMaui/Interfaces/IFileService.cs
+++ RoyAppMaui/Interfaces/IFileService.cs
@@ -1,5 +1,10 @@
-﻿namespace RoyAppMaui.Interfaces;
+﻿using RoyAppMaui.Models;
+
+using System.Collections.ObjectModel;
+
+namespace RoyAppMaui.Interfaces;
 public interface IFileService
 {
+    ObservableCollection<Sleep> ParseImportFileData(string selectedFile);
     Task<FileResult?> SelectImportFile();
 }
--- RoyAppMaui/Services/FileService.cs
+++ RoyAppMaui/Services/FileService.cs
@@ -1,13 +1,48 @@
-﻿using RoyAppMaui.Interfaces;
+﻿using Microsoft.VisualBasic.FileIO;
+
+using RoyAppMaui.Interfaces;
+using RoyAppMaui.Models;
+
+using System.Collections.ObjectModel;
 
 namespace RoyAppMaui.Services;
-public class FileService : IFileService
+public class FileService(IDateTimeService dateTimeService) : IFileService
 {
     private static readonly string[] _ios = ["public.comma-separated-values-text"];
     private static readonly string[] _android = ["text/comma-separated-values"];
     private static readonly string[] _win = [".csv"];
     private static readonly string[] _tizen = ["*/*"];
     private static readonly string[] _mac = ["UTType.commaSeparatedText"];
+
+    public ObservableCollection<Sleep> ParseImportFileData(string selectedFile)
+    {
+        var items = new ObservableCollection<Sleep>();
+        using var parser = new TextFieldParser(selectedFile);
+        parser.TextFieldType = FieldType.Delimited;
+        parser.SetDelimiters(",");
+        while (!parser.EndOfData)
+        {
+            var sleep = new Sleep();
+            var fields = parser.ReadFields();
+            if (fields != null && fields.Length == 3)
+            {
+                sleep.Id = fields[0];
+
+                sleep.Bedtime = dateTimeService.StringToTimeSpan(fields[1]);
+                sleep.BedtimeRec = dateTimeService.TimeSpanToDecimal(sleep.Bedtime);
+                sleep.BedtimeDisplay = dateTimeService.TimeSpanToDateTime((TimeSpan)sleep.Bedtime);
+
+                sleep.Waketime = dateTimeService.StringToTimeSpan(fields[2]);
+                sleep.WaketimeRec = dateTimeService.TimeSpanToDecimal(sleep.Waketime);
+                sleep.WaketimeDisplay = dateTimeService.TimeSpanToDateTime((TimeSpan)sleep.Waketime);
+
+                sleep.Duration = dateTimeService.GetDuration(sleep.BedtimeRec, sleep.WaketimeRec);
+
+                items.Add(sleep);
+            }
+        }
+        return items;
+    }
 
     public async Task<FileResult?> SelectImportFile()
     {
