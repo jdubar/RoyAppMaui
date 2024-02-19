81a9cf7527e298fc330c794b243a3b16f929c3c2
jdubar <55170139+jdubar@users.noreply.github.com>
2024-02-19 10:54:19 -0500

    Added code behind for ConfirmDelete

---
+++ RoyAppMaui/Components/Modals/ConfirmDelete.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using MudBlazor;
+
+namespace RoyAppMaui.Components.Modals;
+public partial class ConfirmDelete
+{
+    [CascadingParameter] MudDialogInstance MudDialog { get; set; } = new();
+
+    [Parameter] public string ContentText { get; set; } = string.Empty;
+
+    private void Cancel() =>
+        MudDialog.Cancel();
+
+    private void Submit() =>
+        MudDialog.Close(DialogResult.Ok(true));
+}
