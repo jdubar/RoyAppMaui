b8dfb8f9091fffaf815e7958a4b33b2de4727e16
jdubar <55170139+jdubar@users.noreply.github.com>
2024-04-14 09:00:28 -0400

    Created main menu component and added custom menu style

--- RoyAppMaui/Components/Layout/MainLayout.razor.cs
+++ RoyAppMaui/Components/Layout/MainLayout.razor.cs
@@ -2,14 +2,12 @@
 
 using MudBlazor;
 
-using RoyAppMaui.Enums;
 using RoyAppMaui.Services;
 
 namespace RoyAppMaui.Components.Layout;
 public partial class MainLayout
 {
     [Inject] private ISettingsService Settings { get; set; } = default!;
-    [Inject] private NotifyService NotifyService { get; set; } = default!;
 
     private MudThemeProvider _mudThemeProvider = default!;
 
@@ -28,15 +26,6 @@ public partial class MainLayout
             StateHasChanged();
         }
     }
-
-    private void HandleClearListClick() =>
-        NotifyService.NotifyOnEventClick(this, new MenuItemClickEventArgs(MenuItems.Clear));
-
-    private void HandleFileExportClick() =>
-        NotifyService.NotifyOnEventClick(this, new MenuItemClickEventArgs(MenuItems.Export));
-
-    private void HandleFileImportClick() =>
-        NotifyService.NotifyOnEventClick(this, new MenuItemClickEventArgs(MenuItems.Import));
 
     private Task OnSystemPreferenceChanged(bool newValue)
     {
---
+++ RoyAppMaui/Components/Layout/MainMenu.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using RoyAppMaui.Enums;
+using RoyAppMaui.Services;
+
+namespace RoyAppMaui.Components.Layout;
+public partial class MainMenu
+{
+    [Inject] private NotifyService NotifyService { get; set; } = default!;
+
+    private void HandleClearListClick() =>
+        NotifyService.NotifyOnEventClick(this, new MenuItemClickEventArgs(MenuItems.Clear));
+
+    private void HandleFileExportClick() =>
+        NotifyService.NotifyOnEventClick(this, new MenuItemClickEventArgs(MenuItems.Export));
+
+    private void HandleFileImportClick() =>
+        NotifyService.NotifyOnEventClick(this, new MenuItemClickEventArgs(MenuItems.Import));
+}
