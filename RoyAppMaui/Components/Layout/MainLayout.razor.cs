92e58295a315700eb9f78c12d6abedd2b8af19c2
jdubar <55170139+jdubar@users.noreply.github.com>
2025-08-08 12:22:52 -0400

    moved MainMenu code into MainLayout

--- RoyAppMaui/Components/Layout/MainLayout.razor.cs
+++ RoyAppMaui/Components/Layout/MainLayout.razor.cs
@@ -3,7 +3,9 @@
 namespace RoyAppMaui.Components.Layout;
 public partial class MainLayout
 {
+    [Inject] private IFileService FileService { get; set; } = default!;
     [Inject] private ISettingsService Settings { get; set; } = default!;
+    [Inject] private ISnackbar Snackbar { get; set; } = default!;
 
     private MudThemeProvider _mudThemeProvider = default!;
 
@@ -14,6 +16,9 @@ public partial class MainLayout
                                            ? MudBlazor.Color.Inherit
                                            : MudBlazor.Color.Warning;
 
+    public string ImportFilePath { get; set; } = default!;
+    public bool IsClearDataGrid { get; set; }
+
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender)
@@ -21,6 +26,27 @@ public partial class MainLayout
             await _mudThemeProvider.WatchSystemDarkModeAsync(OnSystemPreferenceChanged);
             StateHasChanged();
         }
+    }
+
+    private void OnClearGridClick() => IsClearDataGrid = true;
+
+    private void OnExportDataClick() => FileService.RequestExport();
+
+    private async Task OnImportDataClick()
+    {
+        var result = await FileService.SelectImportFile();
+        if (result is null)
+        {
+            return;
+        }
+
+        if (!result.FileName.EndsWith("csv", StringComparison.OrdinalIgnoreCase))
+        {
+            _ = Snackbar.Add("The selected file is not a CSV file!", Severity.Error);
+            return;
+        }
+
+        ImportFilePath = result.FullPath;
     }
 
     private Task OnSystemPreferenceChanged(bool newValue)
