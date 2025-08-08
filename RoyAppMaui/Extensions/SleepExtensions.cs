48f4a88550a20ecbfd91a7efce17790166b3b5c3
jdubar <55170139+jdubar@users.noreply.github.com>
2025-08-08 12:24:42 -0400

    added extensions

---
+++ RoyAppMaui/Extensions/SleepExtensions.cs
@@ -0,0 +1,15 @@
+﻿using RoyAppMaui.Models;
+
+namespace RoyAppMaui.Extensions;
+public static class SleepExtensions
+{
+    public static decimal GetAverage(this IEnumerable<Sleep> sleeps, Func<Sleep, decimal> selector)
+    {
+        if (!sleeps.Any())
+        {
+            return 0m;
+        }
+
+        return decimal.Round(sleeps.ToList().Sum(selector) / sleeps.Count(), 2);
+    }
+}
