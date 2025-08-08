e2123399244a87c1cd8ad11aa266d270281762d1
jdubar <55170139+jdubar@users.noreply.github.com>
2025-08-08 12:24:21 -0400

    updated Add/Update sleep item dialog to now use a custom overlay

--- RoyAppMaui/Components/Pages/SleepTable.razor.cs
+++ RoyAppMaui/Components/Pages/SleepTable.razor.cs
@@ -1,24 +1,30 @@
 ﻿using RoyAppMaui.Components.Modals;
 using RoyAppMaui.Enums;
+using RoyAppMaui.Extensions;
 using RoyAppMaui.Models;
 using RoyAppMaui.Services;
+using RoyAppMaui.Utilities;
 
 using System.Collections.ObjectModel;
 
 namespace RoyAppMaui.Components.Pages;
 public partial class SleepTable
 {
-    [Inject] private IDataService DataService { get; set; } = default!;
+    [CascadingParameter] public string ImportFilePath { get; set; } = string.Empty;
+    [CascadingParameter] public bool IsClearDataGrid { get; set; }
     [Inject] private IDialogService DialogService { get; set; } = default!;
     [Inject] private IFileService FileService { get; set; } = default!;
     [Inject] private ISettingsService Settings { get; set; } = default!;
     [Inject] private ISnackbar Snackbar { get; set; } = default!;
-    [Inject] private NotifyService NotifyService { get; set; } = default!;
 
     private ObservableCollection<Sleep> _items = [];
-    private Sleep _sleep = new();
+    private Sleep _selectedItem = new();
+
+    private MudForm itemForm = new();
+    private bool isValidItem = true;
 
     private bool _isLoading;
+    private bool _showModifyItemDialog;
 
     private decimal BedtimeAvg {  get; set; }
     private decimal WaketimeAvg {  get; set; }
@@ -26,10 +32,30 @@ public partial class SleepTable
     protected override void OnInitialized()
     {
         base.OnInitialized();
-        NotifyService.EventClick += OnMenuItemClick;
+        FileService.OnExportRequested += async () => await ExportTableDataAsync();
     }
 
-    private void AddNewItem() => _items.Add(new Sleep());
+    protected override Task OnParametersSetAsync()
+    {
+        if (ImportFilePath is not null)
+        {
+            _ = ImportFileDataAsync(ImportFilePath);
+            ImportFilePath = string.Empty;
+        }
+
+        if (IsClearDataGrid)
+        {
+            ClearTable();
+            IsClearDataGrid = false;
+        }
+        return base.OnParametersSetAsync();
+    }
+
+    private void ShowAddNewItemOverlay()
+    {
+        _showModifyItemDialog = true;
+        _selectedItem = new Sleep();
+    }
 
     private void ClearTable()
     {
@@ -40,7 +66,7 @@ public partial class SleepTable
 
     private void HandleTimeChange(TimePickers timepicker, TimeSpan? newTime)
     {
-        if (newTime == null)
+        if (newTime is null)
         {
             return;
         }
@@ -48,70 +74,76 @@ public partial class SleepTable
         switch (timepicker)
         {
             case TimePickers.Bedtime:
-                _sleep.Bedtime = newTime;
+                _selectedItem.Bedtime = newTime;
                 break;
             case TimePickers.Waketime:
-                _sleep.Waketime = newTime;
+                _selectedItem.Waketime = newTime;
                 break;
             default:
                 return;
         }
-        _sleep.Duration = DataService.GetDuration(_sleep.BedtimeRec, _sleep.WaketimeRec);
+        _selectedItem.Duration = SleepUtilities.GetSleepDuration(_selectedItem.BedtimeRec, _selectedItem.WaketimeRec);
     }
 
-    private async Task ImportFileDataAsync()
+    private async Task ImportFileDataAsync(string filePath)
     {
-        var selectedFile = await FileService.SelectImportFile();
-        if (selectedFile != null && selectedFile.FileName.EndsWith("csv", StringComparison.OrdinalIgnoreCase))
+        _isLoading = true;
+        ClearTable();
+
+        _items = FileService.ParseImportFileData(filePath);
+        if (_items is null)
         {
-            _isLoading = true;
-            ClearTable();
-            _items = FileService.ParseImportFileData(selectedFile.FullPath);
-            if (_items is not null)
-            {
-                if (_items.Count > 0)
-                {
-                    SetAveragesInView();
-                    _isLoading = false;
-                    await InvokeAsync(StateHasChanged);
-                }
-                else
-                {
-                    _ = Snackbar.Add("No items were imported, check the import file", Severity.Info);
-                }
-            }
-            else
-            {
-                _ = Snackbar.Add("Error! The items list was null", Severity.Error);
-            }
+            _ = Snackbar.Add("Error! The items list was null", Severity.Error);
             _isLoading = false;
+            return;
         }
+
+        if (_items.Count > 0)
+        {
+            SetAveragesInView();
+        }
+        else
+        {
+            _ = Snackbar.Add("No items were imported, check the import file", Severity.Info);
+        }
+
+        _isLoading = false;
+        await InvokeAsync(StateHasChanged);
     }
+
+    private void OnCloseOverlay() => _showModifyItemDialog = false;
 
     private void OnCommittedItemChanges(Sleep sleep) => SetAveragesInView();
 
-    private void OnMenuItemClick(object? sender, EventArgs e)
+    private void OnModifyItem(Sleep item)
     {
-        switch (((MenuItemClickEventArgs)e).MenuItem)
+        _selectedItem = item;
+        _showModifyItemDialog = true;
+    }
+
+    private void OnSaveBedtimeItem()
+    {
+        itemForm.Validate();
+        if (itemForm.IsValid)
         {
-            case MenuItems.Import:
-                _ = Task.Run(ImportFileDataAsync);
-                break;
-            case MenuItems.Export:
-                _ = Task.Run(SaveFileDataAsync);
-                break;
-            case MenuItems.Clear:
-                ClearTable();
-                break;
+            _showModifyItemDialog = false;
+            if (_items.Contains(_selectedItem))
+            {
+                var index = _items.IndexOf(_selectedItem);
+                _items[index] = _selectedItem;
+            }
+            else
+            {
+                _items.Add(_selectedItem);
+            }
+
+            SetAveragesInView();
         }
-        InvokeAsync(StateHasChanged);
     }
 
     private void OnRowsPerPageChanged(int pageSize) => Settings.RowsPerPage = pageSize;
 
-    private void OnStartedEditingItem(Sleep item) => _sleep = item;
-
-    private async Task SaveFileDataAsync()
+    private async Task ExportTableDataAsync()
     {
         if (_items.Count < 1)
         {
@@ -119,7 +151,8 @@ public partial class SleepTable
             return;
         }
 
-        _ = await FileService.SaveDataToFile(DataService.GetExportData(_items))
+        var data = FileService.GetExportData(_items);
+        _ = await FileService.SaveDataToFile(data)
             ? Snackbar.Add("Successfully saved the file", Severity.Success)
             : Snackbar.Add("Error saving the file!", Severity.Error);
     }
@@ -146,17 +179,17 @@ public partial class SleepTable
             return;
         }
 
-        BedtimeAvg = DataService.GetAverageOfBedtimes(_items);
-        WaketimeAvg = DataService.GetAverageOfWaketimes(_items);
+        BedtimeAvg = _items.GetAverage(s => s.BedtimeRec);
+        WaketimeAvg = _items.GetAverage(s => s.WaketimeRec);
     }
 
     private async Task<DialogResult> ShowConfirmDeleteDialogAsync()
     {
         var dialog = await DialogService.ShowAsync<ConfirmDelete>("Delete");
-        var result = dialog.Result;
+        var result = await dialog.Result;
 
-        return !result.IsCanceled
-            ? DialogResult.Ok(true)
-            : DialogResult.Cancel();
+        return result is null || result.Canceled
+            ? DialogResult.Cancel()
+            : DialogResult.Ok(true);
     }
 }
