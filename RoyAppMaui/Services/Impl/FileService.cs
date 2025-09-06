dee766db2d4096142b5a8ab70b9295399ed2d56e
jdubar <55170139+jdubar@users.noreply.github.com>
2025-09-06 10:35:48 -0400

    updated FileService to be more robust

--- RoyAppMaui/Services/Impl/FileService.cs
+++ RoyAppMaui/Services/Impl/FileService.cs
@@ -8,12 +8,18 @@ using RoyAppMaui.Extensions;
 using RoyAppMaui.Models;
 
 using System.Globalization;
+using System.Text;
 
 namespace RoyAppMaui.Services.Impl;
 public class FileService(System.IO.Abstractions.IFileSystem fileSystem) : IFileService
 {
     public Result<List<Sleep>> GetSleepDataFromCsv(string filePath)
     {
+        if (string.IsNullOrWhiteSpace(filePath))
+        {
+            return Result.Fail("File path is null or empty.");
+        }
+
         var config = new CsvConfiguration(CultureInfo.InvariantCulture)
         {
             HasHeaderRecord = false
@@ -26,7 +32,7 @@ public class FileService(System.IO.Abstractions.IFileSystem fileSystem) : IFileS
             csv.Context.RegisterClassMap<SleepMap>();
             var records = csv.GetRecords<Sleep>().ToList();
 
-            return records.Count != 0
+            return records.Count > 0
                 ? Result.Ok(records)
                 : Result.Fail("No sleep records found in the CSV file.");
         }
@@ -47,15 +53,17 @@ public class FileService(System.IO.Abstractions.IFileSystem fileSystem) : IFileS
     public string GetExportData(IEnumerable<Sleep> sleeps)
     {
         const string header = "Id,Bedtime,Bedtime (as decimal),Waketime,Waketime (as decimal),Duration";
-        var csvsleep = sleeps.Select(s =>
-        {
-            return $"{s.Id},{s.BedtimeDisplay},{s.BedtimeRec},{s.WaketimeDisplay},{s.WaketimeRec},{s.Duration}";
-        });
-        var data = string.Join(Environment.NewLine, csvsleep);
-        var footer = $"{Environment.NewLine}Bedtime Average: {sleeps.GetAverage(s => s.BedtimeRec)}" +
-                     $"{Environment.NewLine}Waketime Average: {sleeps.GetAverage(s => s.WaketimeRec)}" +
-                     $"{Environment.NewLine}Duration Average: {sleeps.GetAverage(s => s.Duration)}";
 
-        return $"{header}{Environment.NewLine}{data}{Environment.NewLine}{footer}";
+        var sb = new StringBuilder();
+        sb.AppendLine(header);
+        foreach (var sleep in sleeps)
+        {
+            sb.AppendLine($"{sleep.Id},{sleep.BedtimeDisplay},{sleep.BedtimeRec},{sleep.WaketimeDisplay},{sleep.WaketimeRec},{sleep.Duration}");
+        }
+
+        sb.AppendLine($"Bedtime Average: {sleeps.GetAverage(s => s.BedtimeRec)}");
+        sb.AppendLine($"Waketime Average: {sleeps.GetAverage(s => s.WaketimeRec)}");
+        sb.AppendLine($"Duration Average: {sleeps.GetAverage(s => s.Duration)}");
+        return sb.ToString();
     }
 }
