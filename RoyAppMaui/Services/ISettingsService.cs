ef010c7567da2143b52fcbbf72a10e57bed609be
jdubar <55170139+jdubar@users.noreply.github.com>
2025-09-15 11:13:09 -0400

    now saving window size to settings

--- RoyAppMaui/Services/ISettingsService.cs
+++ RoyAppMaui/Services/ISettingsService.cs
@@ -3,4 +3,6 @@ public interface ISettingsService
 {
     bool IsDarkMode { get; set; }
     int RowsPerPage { get; set; }
+    double WindowHeight { get; set; }
+    double WindowWidth { get; set; }
 }
