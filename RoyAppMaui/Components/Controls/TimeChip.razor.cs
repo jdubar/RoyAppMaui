9c852e4dd417f2be9ceca9e48404cc8c4d126deb
jdubar <55170139+jdubar@users.noreply.github.com>
2025-09-16 16:29:37 -0400

    added custom controls for better code reuse

---
+++ RoyAppMaui/Components/Controls/TimeChip.razor.cs
@@ -0,0 +1,20 @@
+﻿using RoyAppMaui.Extensions;
+
+namespace RoyAppMaui.Components.Controls;
+public partial class TimeChip
+{
+    [Parameter] public decimal Time { get; set; } = 0m;
+
+    public string TimeDisplay => Time.ToFormattedString();
+    public string Icon { get; set; } = Icons.Material.Filled.WbSunny;
+    public MudBlazor.Color IconColor { get; set; } = MudBlazor.Color.Warning;
+
+    protected override void OnParametersSet()
+    {
+        if (Time is >= 20m || Time is < 6m)
+        {
+            Icon = Icons.Material.Filled.NightsStay;
+            IconColor = MudBlazor.Color.Info;
+        }
+    }
+}
---
+++ RoyAppMaui/Components/Controls/TimePicker.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace RoyAppMaui.Components.Controls;
+public partial class TimePicker
+{
+    [Parameter] public string Icon { get; set; } = Icons.Material.Filled.AccessTime;
+    [Parameter] public string Label { get; set; } = string.Empty;
+    [Parameter] public TimeSpan? SleepTime { get; set; } = TimeSpan.Zero;
+    [Parameter] public EventCallback<TimeSpan?> SleepTimeChanged { get; set; }
+
+    public MudTimePicker _sleepTimePicker = default!;
+
+    private async void HandleTimeChange(TimeSpan? newTime)
+    {
+        SleepTime = newTime;
+        await SleepTimeChanged.InvokeAsync(newTime);
+    }
+}
