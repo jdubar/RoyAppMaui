277fbabe8fcffef83d522f5f1355b11887879422
jdubar <55170139+jdubar@users.noreply.github.com>
2025-09-17 07:55:39 -0400

    fixed tests

--- RoyAppMaui.Tests/Services/DataServiceTests.cs
+++ RoyAppMaui.Tests/Services/DataServiceTests.cs
@@ -43,7 +43,7 @@ public class DataServiceTests
         // Assert
         var actual = Encoding.UTF8.GetString(result);
         Assert.Contains("Id,Bedtime,Bedtime (as decimal),Waketime,Waketime (as decimal)", actual);
-        Assert.Contains("1,10:00 PM,22,06:00 AM,6", actual);
+        Assert.Contains("1,10:00 PM,22.00,06:00 AM,06.00", actual);
         Assert.Contains("Bedtime Average: 22", actual);
         Assert.Contains("Waketime Average: 6", actual);
     }
@@ -55,8 +55,8 @@ public class DataServiceTests
         var service = new DataService();
         var sleeps = new List<Sleep>
         {
-            new() { Id = "1", Bedtime = "10:00 PM".ToTimeSpan(), Waketime = "06:00 AM".ToTimeSpan() },
-            new() { Id = "2", Bedtime = "11:00 PM".ToTimeSpan(), Waketime = "07:00 AM".ToTimeSpan() }
+            new() { Id = "1", Bedtime = new TimeSpan(22, 0, 0), Waketime = new TimeSpan(6, 0, 0) },
+            new() { Id = "2", Bedtime = new TimeSpan(23, 0, 0), Waketime = new TimeSpan(7, 0, 0) }
         };
 
         // Act
@@ -64,8 +64,8 @@ public class DataServiceTests
 
         // Assert
         var actual = Encoding.UTF8.GetString(result);
-        Assert.Contains("1,10:00 PM,22,06:00 AM,6", actual);
-        Assert.Contains("2,11:00 PM,23,07:00 AM,7", actual);
+        Assert.Contains("1,10:00 PM,22.00,06:00 AM,06.00", actual);
+        Assert.Contains("2,11:00 PM,23.00,07:00 AM,07.00", actual);
         Assert.Contains("Bedtime Average: 22.5", actual);
         Assert.Contains("Waketime Average: 6.5", actual);
     }
